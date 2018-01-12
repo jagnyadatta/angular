01640f3e5adaef57f6e7b4b156639915d245e30f
jagnya <jagnya.datta@sonata-software.com>
2018-01-12 16:20:46 +0530

    Lecture -53

--- AgularProject/src/app/shopping-list/shopping-list.component.ts
+++ AgularProject/src/app/shopping-list/shopping-list.component.ts
@@ -6,7 +6,9 @@ import { Ingrediant } from '../shared/ingrediant.modal';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingrediants:Ingrediant[]=[
+  new Ingrediant('Apple',5),   new Ingrediant('Pineapple',5),  new Ingrediant('Tomato',5),  new Ingrediant('Papaya',5),  new Ingrediant('Apple',5),
+];
     constructor() { }
 
   ngOnInit() {
